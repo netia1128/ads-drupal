8bfc1d56b55d91ab5e36d00c9259bb7c66251c8f
netia1128 <netia1128@hotmail.com>
2021-06-20 15:23:06 -0600

    Initial commit

---
+++ sites/default/files/php/twig/60cf741a13c03_update-last-check.html.tw_BAApAHj_mHLlcN5nJN_eWhVUW/IQLbFSMcNXN-04FFPPZcCanwKLYhC14HAQtqD5Yc7Y0.php
@@ -0,0 +1,110 @@
+<?php
+
+use Twig\Environment;
+use Twig\Error\LoaderError;
+use Twig\Error\RuntimeError;
+use Twig\Extension\SandboxExtension;
+use Twig\Markup;
+use Twig\Sandbox\SecurityError;
+use Twig\Sandbox\SecurityNotAllowedTagError;
+use Twig\Sandbox\SecurityNotAllowedFilterError;
+use Twig\Sandbox\SecurityNotAllowedFunctionError;
+use Twig\Source;
+use Twig\Template;
+
+/* core/themes/stable/templates/admin/update-last-check.html.twig */
+class __TwigTemplate_69ebf02e5fe8e3a3f25b710c6a143cde31ee18fb8ea5a0d28728972da277e059 extends \Twig\Template
+{
+    private $source;
+    private $macros = [];
+
+    public function __construct(Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->source = $this->getSourceContext();
+
+        $this->parent = false;
+
+        $this->blocks = [
+        ];
+        $this->sandbox = $this->env->getExtension('\Twig\Extension\SandboxExtension');
+        $this->checkSecurity();
+    }
+
+    protected function doDisplay(array $context, array $blocks = [])
+    {
+        $macros = $this->macros;
+        // line 14
+        echo "<p>
+  ";
+        // line 15
+        if (($context["last"] ?? null)) {
+            // line 16
+            echo "    ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Last checked: @time ago", ["@time" => ($context["time"] ?? null)]));
+            echo "
+  ";
+        } else {
+            // line 18
+            echo "    ";
+            echo $this->extensions['Drupal\Core\Template\TwigExtension']->renderVar(t("Last checked: never"));
+            echo "
+  ";
+        }
+        // line 20
+        echo "  (";
+        echo $this->extensions['Drupal\Core\Template\TwigExtension']->escapeFilter($this->env, $this->sandbox->ensureToStringAllowed(($context["link"] ?? null), 20, $this->source), "html", null, true);
+        echo ")
+</p>
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "core/themes/stable/templates/admin/update-last-check.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  56 => 20,  50 => 18,  44 => 16,  42 => 15,  39 => 14,);
+    }
+
+    public function getSourceContext()
+    {
+        return new Source("", "core/themes/stable/templates/admin/update-last-check.html.twig", "/var/www/html/drupal/core/themes/stable/templates/admin/update-last-check.html.twig");
+    }
+    
+    public function checkSecurity()
+    {
+        static $tags = array("if" => 15);
+        static $filters = array("t" => 16, "escape" => 20);
+        static $functions = array();
+
+        try {
+            $this->sandbox->checkSecurity(
+                ['if'],
+                ['t', 'escape'],
+                []
+            );
+        } catch (SecurityError $e) {
+            $e->setSourceContext($this->source);
+
+            if ($e instanceof SecurityNotAllowedTagError && isset($tags[$e->getTagName()])) {
+                $e->setTemplateLine($tags[$e->getTagName()]);
+            } elseif ($e instanceof SecurityNotAllowedFilterError && isset($filters[$e->getFilterName()])) {
+                $e->setTemplateLine($filters[$e->getFilterName()]);
+            } elseif ($e instanceof SecurityNotAllowedFunctionError && isset($functions[$e->getFunctionName()])) {
+                $e->setTemplateLine($functions[$e->getFunctionName()]);
+            }
+
+            throw $e;
+        }
+
+    }
+}
